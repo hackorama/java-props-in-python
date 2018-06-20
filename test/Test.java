23f3f3e003b09b79510866dc57cb7e8d4b080424
hackorama <kishan.thomas@gmail.com>
2018-06-19 19:53:13 -0700

    Escaping special characters in the json output

--- test/Test.java
+++ test/Test.java
@@ -5,7 +5,18 @@ import java.util.Properties;
 import java.util.TreeMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
+/**
+ * Read properties and sort by key so it can be compared with the Python jprops output
+ * Format output as valid JSON (escape quotes) to match the json output from Python dict
+ */
 public class Test {
+    public static String jsonEscape(String value) {
+        String escaped = value;
+        escaped = escaped.replaceAll("\"", "\\\\\"");
+        escaped = escaped.replaceAll("\t", "\\\\t");
+        escaped = escaped.replaceAll("\f", "\\\\f");
+        return escaped;
+    }
     public static void main(String[] args) {
         try (InputStream inputStream = new FileInputStream("test.properties")) {
             Properties properties = new Properties();
@@ -14,7 +25,8 @@ public class Test {
             properties.forEach((key, value) -> orderedProperties.put((String) key, (String) value));
             System.out.println("{");
             final AtomicInteger counter = new AtomicInteger(1);
-            orderedProperties.forEach((key, value) -> System.out.format(" \"%s\": \"%s\"%s%n", key, value,
+            orderedProperties.forEach((key, value) -> System.out.format(" \"%s\": \"%s\"%s%n",
+                    jsonEscape(key), jsonEscape(value),
                     (counter.getAndIncrement() == orderedProperties.size()) ? "" : ", "));
             System.out.println("}");
         } catch (IOException e) {
